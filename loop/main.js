0cd0f6444880de805e7376e659030b984cbd8e69
unknown <diana.chorna@gmail.com>
2022-01-23 18:25:45 +0200

    loop

---
+++ loop/main.js
@@ -0,0 +1,59 @@
+// завдання 1
+let n = prompt('Input n')
+let result =0;
+for(let i =1; i<n; i++){
+      result*=n;
+      console.log(result);
+}
+
+//завдання  2 +
+// for(let i = 1000; i<=9999; i+=3){
+//       console.log(i);
+// }
+
+// завдання 3 +
+// let count =0;
+// for(let i = 1; i<1000; i+=2){
+//       count++;
+//       if(count<=55){
+//             console.log(i);
+//       }
+// }
+
+//завдання 4 +
+// for(a=90; a>=0; a-=5){
+//       console.log(a);
+// }
+
+//завдання 5 +
+// let count =0;
+// for(let i = 2; i<1000; i+=2){
+//       count++;
+//       if(count<=20){
+//             console.log(i);
+//       }
+// }
+
+//завдання 6 +
+// for(a=2; a<=1000; a=2*a-1){
+//       console.log(a);
+// }
+
+//завдання 7 +
+// for(a=-166; a<=99; a=2*a+200){
+//       if(a>=-99 && a<=-10){
+//             console.log(a);
+//       }
+//       else if(a>=10){
+//             console.log(a);
+//       }
+// }
+
+//завдання 8
+// let n = +prompt('Input n');
+// let step = +prompt('Input step')
+// let result =1;
+// for(let i =1; i<=step; i++){
+//       result*=n;
+// }
+// console.log(result);
